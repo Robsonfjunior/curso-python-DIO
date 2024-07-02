2828a270819fbfab1537f848dde26a62145556d4
Robson Junior <157227786+Robsonfjunior@users.noreply.github.com>
2024-07-01 23:35:05 -0300

    estudo_01-07

---
+++ 02- POO/Manipulação_de_arquivo/02_os_shurtil.py
@@ -0,0 +1,12 @@
+import os
+import shutil
+from pathlib import Path
+
+ROOT_PATH = Path(__file__).parent
+
+# os.mkdir(ROOT_PATH / 'novo-diretorio')
+
+arquivo = open(ROOT_PATH / "novo.txt", "w")
+arquivo.close()
+
+os.rename(ROOT_PATH/"novo.txt", ROOT_PATH/"alterado.txt")
